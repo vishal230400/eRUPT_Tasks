412c286f0f5fbdde11665101083db9f8e9cc74db
Vishal Sankar Ram <vishals230400@gmail.com>
2025-01-09 03:17:50 +0000

    single_get_range

---
+++ task3/build/CMakeFiles/SingleGetRange.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SingleGetRange.dir/single_get_range.c.o"
+  "SingleGetRange"
+  "SingleGetRange.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SingleGetRange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
