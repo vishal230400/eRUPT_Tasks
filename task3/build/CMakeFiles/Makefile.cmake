f9d75cc816f3ae7805bbf28a2d6a1d5a52954601
Vishal Sankar Ram <vishals230400@gmail.com>
2025-01-09 03:51:18 +0000

    singleVMulti

--- task3/build/CMakeFiles/Makefile.cmake
+++ task3/build/CMakeFiles/Makefile.cmake
@@ -45,6 +45,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/SingleVSMultiRange.dir/DependInfo.cmake"
   "CMakeFiles/SingleGetRange.dir/DependInfo.cmake"
   "CMakeFiles/BasicFDBOps.dir/DependInfo.cmake"
   )
---
+++ task3/build/CMakeFiles/SingleVSMultiRange.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SingleVSMultiRange.dir/single_vs_multi_ranges.c.o"
+  "SingleVSMultiRange"
+  "SingleVSMultiRange.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SingleVSMultiRange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
